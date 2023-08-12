981909ae9372c55c135351d5499b5d9056cbf465
David Garcia <davidgarcia27737@gmail.com>
2023-08-12 18:42:45 -0500

    Add: Se crea ambiente capaciter.

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'platzi-intro-react-base',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
